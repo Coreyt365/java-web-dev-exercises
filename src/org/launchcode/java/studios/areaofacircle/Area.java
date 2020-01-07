af329367b4712e72f9a8546929adc832e6d78b18
Coreyt365 <coritye@gmail.com>
2020-01-06 19:41:37 -0600

    Class 1 exercise and studio added the bonus for studio

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -4,9 +4,18 @@ public class Area {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the radius of your circle:");
+        while(  !input.hasNextInt()){
+            input.next();
+            System.out.println("Please enter an integer: ");
+        }
 
         double radius = input.nextDouble();
+        while (radius <= 0){
+            System.out.println("Please enter a positive integer: ");
+            radius = input.nextDouble();
+        }
         double area =Circle.getArea(radius);
+
         System.out.println("Your circle area is "+ area);
         input.close();
     }
